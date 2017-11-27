1ac6fd2adde29713a2c3ee02f970a238609b4986
Thorsten Wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2017-11-27 14:52:30 +0100

    GUI don't freeze when press apply.

--- src/main/java/de/mpi_dortmund/ij/mpitools/helicalPicker/gui/ApplyActionListener.java
+++ src/main/java/de/mpi_dortmund/ij/mpitools/helicalPicker/gui/ApplyActionListener.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.swing.SwingWorker;
+
 import de.mpi_dortmund.ij.mpitools.FilamentEnhancer.FilamentEnhancerContext;
 import de.mpi_dortmund.ij.mpitools.boxplacer.BoxPlacer_;
 import de.mpi_dortmund.ij.mpitools.boxplacer.BoxPlacingContext;
@@ -22,37 +24,49 @@ public class ApplyActionListener implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		HelicalPickerGUI gui = Helical_Picker_.getGUI();
 		
-		ImagePlus input_image = Helical_Picker_.getInstance().getImage();
-		
-		PipelineRunner runner = new PipelineRunner();
-		int sliceFrom = 1;
-		int sliceTo = input_image.getStackSize();
-		SliceRange slice_range = new SliceRange(sliceFrom, sliceTo);
-		FilamentFilterContext skeleton_filter_context = gui.getLineFilterContext();
-		FilamentDetectorContext detector_context = gui.getFilamentDetectorContext();
-		FilamentEnhancerContext enhancer_context = gui.getFilamentEnhancerContext();
+		SwingWorker<Boolean, Integer> worker = new SwingWorker<Boolean, Integer>(){
+			
+			@Override
+			protected Boolean doInBackground() throws Exception {
+				HelicalPickerGUI gui = Helical_Picker_.getGUI();
+				
+				ImagePlus input_image = Helical_Picker_.getInstance().getImage();
+				
+				PipelineRunner runner = new PipelineRunner();
+				int sliceFrom = 1;
+				int sliceTo = input_image.getStackSize();
+				SliceRange slice_range = new SliceRange(sliceFrom, sliceTo);
+				FilamentFilterContext skeleton_filter_context = gui.getLineFilterContext();
+				FilamentDetectorContext detector_context = gui.getFilamentDetectorContext();
+				FilamentEnhancerContext enhancer_context = gui.getFilamentEnhancerContext();
 
-		runner.run(input_image,slice_range, skeleton_filter_context,enhancer_context,detector_context);
-	
-		HashMap<Integer, ArrayList<Polygon>> filtered_lines = runner.getFilteredLines();
-		int box_size = Integer.parseInt(gui.textfieldBoxSize.getText());
-		int box_distance = Integer.parseInt(gui.textfieldBoxDistance.getText());
-		/*
-		 * Place boxes
-		 */
-		CentralLog.getInstance().info("Place boxes");
-		boolean place_points = false;
-		BoxPlacingContext placing_context = new BoxPlacingContext();
-		placing_context.setBoxSize(box_size);
-		placing_context.setBoxDistance(box_distance);
-		placing_context.setPlacePoints(place_points);
+				runner.run(input_image,slice_range, skeleton_filter_context,enhancer_context,detector_context);
+			
+				HashMap<Integer, ArrayList<Polygon>> filtered_lines = runner.getFilteredLines();
+				int box_size = Integer.parseInt(gui.textfieldBoxSize.getText());
+				int box_distance = Integer.parseInt(gui.textfieldBoxDistance.getText());
+				/*
+				 * Place boxes
+				 */
+				CentralLog.getInstance().info("Place boxes");
+				boolean place_points = false;
+				BoxPlacingContext placing_context = new BoxPlacingContext();
+				placing_context.setBoxSize(box_size);
+				placing_context.setBoxDistance(box_distance);
+				placing_context.setPlacePoints(place_points);
+				
+				BoxPlacer_ boxPlacer = new BoxPlacer_();
+				boxPlacer.placeBoxes(filtered_lines, input_image, placing_context);
+				
+				//picker_.placeBoxes(filtered_lines, placing_context);
+				return true;
+			}
+			
+		};
+		worker.execute();
 		
-		BoxPlacer_ boxPlacer = new BoxPlacer_();
-		boxPlacer.placeBoxes(filtered_lines, input_image, placing_context);
 		
-		//picker_.placeBoxes(filtered_lines, placing_context);
 	}
 
 }

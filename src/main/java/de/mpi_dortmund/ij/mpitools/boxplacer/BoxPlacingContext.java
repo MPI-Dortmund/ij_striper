f226f6719caa6cc615c6551103cc0f3dc1df90d7
Thorsten Wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2017-11-28 16:49:04 +0100

    Add documentation

--- src/main/java/de/mpi_dortmund/ij/mpitools/boxplacer/BoxPlacingContext.java
+++ src/main/java/de/mpi_dortmund/ij/mpitools/boxplacer/BoxPlacingContext.java
@@ -13,27 +13,59 @@ public class BoxPlacingContext {
 		place_points = false;
 	}
 	
+	/**
+	 * @return Position where the boxes will be placed
+	 */
 	public int getSlicePosition() {
 		return slicePosition;
 	}
+	
+	/**
+	 * Set the position where the boxes will be placed
+	 */
 	public void setSlicePosition(int slicePosition) {
 		this.slicePosition = slicePosition;
 	}
+	
+	/**
+	 * 
+	 * @return The side length of the boxes to be placed.
+	 */
 	public int getBoxSize() {
 		return box_size;
 	}
+	
+	/** 
+	 * @param box_size The side length of the boxes to be placed.
+	 */
 	public void setBoxSize(int box_size) {
 		this.box_size = box_size;
 	}
+	
+	/**
+	 * @return The distance between two boxes.
+	 */
 	public int getBoxDistance() {
 		return box_distance;
 	}
+	
+	/**
+	 * @param box_distance The distance between two boxes.
+	 */
 	public void setBoxDistance(int box_distance) {
 		this.box_distance = box_distance;
 	}
+	
+	/**
+	 * @return If true, points instead of boxes will be placed.
+	 */
 	public boolean isPlacePoints() {
 		return place_points;
 	}
+	
+	/**
+	 * @param place_points True, if points instead of boxes should be placed.
+	 */
 	public void setPlacePoints(boolean place_points) {
 		this.place_points = place_points;
 	}

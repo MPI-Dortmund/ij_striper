477faf2e63c87dc56c7090045647a05ba86153b8
Thorsten Wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2017-11-30 14:04:39 +0100

    Add unit tests for ridge optimizer worker

---
+++ src/test/java/ij_helical_picker/TestRidgeOptimizerWorker.java
@@ -0,0 +1,21 @@
+package ij_helical_picker;
+
+import static org.junit.Assert.*;
+
+import java.net.URL;
+
+import org.junit.Test;
+
+import de.mpi_dortmund.ij.mpitools.FilamentEnhancer.FilamentEnhancer;
+import de.mpi_dortmund.ij.mpitools.FilamentEnhancer.FilamentEnhancerContext;
+import de.mpi_dortmund.ij.mpitools.RidgeDetectionOptimizer.RidgeOptimizerWorker;
+import de.mpi_dortmund.ij.mpitools.helicalPicker.FilamentDetector.DetectionThresholdRange;
+import de.mpi_dortmund.ij.mpitools.helicalPicker.gui.SliceRange;
+import ij.ImagePlus;
+import ij.ImageStack;
+
+public class TestRidgeOptimizerWorker {
+
+	
+
+}
